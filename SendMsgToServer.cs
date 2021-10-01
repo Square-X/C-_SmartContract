8d4bd8019b7c99fa701261c5ca508ca651962fb1
根号X的四次方 <379779335@qq.com>
2021-10-02 00:00:47 +0800

    fir

---
+++ SendMsgToServer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace mint
+{
+    class SendMsgToServer
+    {
+        /// <summary>
+        /// 向服务器发送信息
+        /// </summary>
+        /// <param name="posturl">请求路径</param>
+        /// <param name="postData">请求参数</param>
+        /// <returns>返回信息</returns>
+        public string SendInfo(string posturl, string postData)
+        {
+            Stream outstream = null;
+            Stream instream = null;
+            StreamReader sr = null;
+            HttpWebResponse response = null;
+            HttpWebRequest request = null;
+            Encoding encoding = Encoding.UTF8;
+            byte[] data = encoding.GetBytes(postData);
+            // 准备请求...
+            try
+            {
+                // 设置参数
+                request = WebRequest.Create(posturl) as HttpWebRequest;
+                CookieContainer cookieContainer = new CookieContainer();
+                request.CookieContainer = cookieContainer;
+                request.AllowAutoRedirect = true;
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+                outstream = request.GetRequestStream();
+                outstream.Write(data, 0, data.Length);
+                outstream.Close();
+                //发送请求并获取相应回应数据
+                response = request.GetResponse() as HttpWebResponse;
+                //直到request.GetResponse()程序才开始向目标网页发送Post请求
+                instream = response.GetResponseStream();
+                sr = new StreamReader(instream, encoding);
+                //返回结果网页（html）代码
+                string content = sr.ReadToEnd();
+                string err = string.Empty;
+                return content;
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ getEthRawTx.cs
@@ -0,0 +1,41 @@
+﻿using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts;
+using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.RPC.TransactionManagers;
+using Nethereum.Signer;
+using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
+using System;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace mint
+{
+    class getEthRawTx
+    {
+        public string from;
+        public HexBigInteger nonce;
+        public HexBigInteger gasLimit;
+        public HexBigInteger gasPrice;
+        public string to;
+        public HexBigInteger value;
+        public string data;
+        public int chainId;
+
+        public getEthRawTx(string fromAddress, string contractAddress, string toAddress, Contract contractObj, int _tokenAmount, int nonceNum, string privKey)
+        {
+            from = fromAddress;
+            nonce = new HexBigInteger(nonceNum);
+            gasLimit = new HexBigInteger(60000000);
+            gasPrice = new HexBigInteger((BigInteger)10e9);
+            to = contractAddress;
+            value = new HexBigInteger(0);
+            data = contractObj.GetFunction("transferFrom").SendTransactionAsync(fromAddress, toAddress, _tokenAmount).ToString();
+            chainId = 80001;
+
+        }
+    }
+}
