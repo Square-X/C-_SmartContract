0dc9b114574748ab1ecdcdf6485d9b1f512d9142
根号X的四次方 <379779335@qq.com>
2021-10-08 19:49:22 +0800

    change

--- Program.cs
+++ Program.cs
@@ -17,13 +17,359 @@ public class Program
 
         var myaddress = "0x8AF41Cacb5FE289587292BA3c7eaa4a65b383c80";
         var privateKey = "91218c9ef59fe4ae8fda0cb0c8086756191f311850e673721822dcabce1c99c9";
-        //var account = new Account(privateKey, 80001);
-        var account = new Account(privateKey, 1);
-        var web3 = new Web3(account, "https://mainnet.infura.io/v3/69c833c16d17444cabe16e9a50c15b6b");
-        //var web3 = new Web3(account, "https://matic-mumbai.chainstacklabs.com");
-        var contractAddress = "0x869ad3dfb0f9acb9094ba85228008981be6dbdde";         //Ethereum mainnet 
-       
-        var abi = @"[{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""chestComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""footComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getChest"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getFoot"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getHand"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getHead"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getNeck"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getRing"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getWaist"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getWeapon"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""handComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""headComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""neckComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""ringComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""tokenURI"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""waistComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""weaponComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""}]";
+
+        var account = new Account(privateKey, 80001);
+        //var account = new Account(privateKey, 1);
+
+        //var web3 = new Web3(account, "https://mainnet.infura.io/v3/69c833c16d17444cabe16e9a50c15b6b");
+        var web3 = new Web3(account, "https://matic-mumbai.chainstacklabs.com");
+
+        //var contractAddress = "0x869ad3dfb0f9acb9094ba85228008981be6dbdde";         //Ethereum mainnet 
+        var contractAddress = "0x646aa826c2adC175C2bEB90432af73ba73521e9A";
+
+        var abi = @"[
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""weaponComponents"",
+      ""outputs"": [
+        {
+                ""internalType"": ""uint256[5]"",
+          ""name"": """",
+          ""type"": ""uint256[5]""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""chestComponents"",
+      ""outputs"": [
+        {
+                ""internalType"": ""uint256[5]"",
+          ""name"": """",
+          ""type"": ""uint256[5]""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""headComponents"",
+      ""outputs"": [
+        {
+                ""internalType"": ""uint256[5]"",
+          ""name"": """",
+          ""type"": ""uint256[5]""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""waistComponents"",
+      ""outputs"": [
+        {
+                ""internalType"": ""uint256[5]"",
+          ""name"": """",
+          ""type"": ""uint256[5]""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""footComponents"",
+      ""outputs"": [
+        {
+                ""internalType"": ""uint256[5]"",
+          ""name"": """",
+          ""type"": ""uint256[5]""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""handComponents"",
+      ""outputs"": [
+        {
+                ""internalType"": ""uint256[5]"",
+          ""name"": """",
+          ""type"": ""uint256[5]""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""neckComponents"",
+      ""outputs"": [
+        {
+                ""internalType"": ""uint256[5]"",
+          ""name"": """",
+          ""type"": ""uint256[5]""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""ringComponents"",
+      ""outputs"": [
+        {
+                ""internalType"": ""uint256[5]"",
+          ""name"": """",
+          ""type"": ""uint256[5]""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""getWeapon"",
+      ""outputs"": [
+        {
+                ""internalType"": ""string"",
+          ""name"": """",
+          ""type"": ""string""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""getChest"",
+      ""outputs"": [
+        {
+                ""internalType"": ""string"",
+          ""name"": """",
+          ""type"": ""string""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""getHead"",
+      ""outputs"": [
+        {
+                ""internalType"": ""string"",
+          ""name"": """",
+          ""type"": ""string""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""getWaist"",
+      ""outputs"": [
+        {
+                ""internalType"": ""string"",
+          ""name"": """",
+          ""type"": ""string""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""getFoot"",
+      ""outputs"": [
+        {
+                ""internalType"": ""string"",
+          ""name"": """",
+          ""type"": ""string""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""getHand"",
+      ""outputs"": [
+        {
+                ""internalType"": ""string"",
+          ""name"": """",
+          ""type"": ""string""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""getNeck"",
+      ""outputs"": [
+        {
+                ""internalType"": ""string"",
+          ""name"": """",
+          ""type"": ""string""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""getRing"",
+      ""outputs"": [
+        {
+                ""internalType"": ""string"",
+          ""name"": """",
+          ""type"": ""string""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    },
+    {
+            ""inputs"": [
+              {
+                ""internalType"": ""address"",
+          ""name"": ""walletAddress"",
+          ""type"": ""address""
+              }
+      ],
+      ""name"": ""tokenURI"",
+      ""outputs"": [
+        {
+                ""internalType"": ""string"",
+          ""name"": """",
+          ""type"": ""string""
+        }
+      ],
+      ""stateMutability"": ""view"",
+      ""type"": ""function"",
+      ""constant"": true
+    }
+  ]";
+        //var abi = @"[{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""chestComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""footComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getChest"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getFoot"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getHand"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getHead"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getNeck"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getRing"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getWaist"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""getWeapon"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""handComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""headComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""neckComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""ringComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""tokenURI"",""outputs"":[{""internalType"":""string"",""name"":"""",""type"":""string""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""waistComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""},{""inputs"":[{""internalType"":""address"",""name"":""walletAddress"",""type"":""address""}],""name"":""weaponComponents"",""outputs"":[{""internalType"":""uint256[5]"",""name"":"""",""type"":""uint256[5]""}],""stateMutability"":""view"",""type"":""function""}]";
         var contract = web3.Eth.GetContract(abi , contractAddress);
 
         var getWeapon = contract.GetFunction("getWeapon");
